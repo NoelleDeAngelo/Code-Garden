9460a6d6f2e4a8208739664b18691da86b8676ad
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2025-11-07 15:01:47 -0800

    Update settings for database on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    outputFileTracingIncludes: {
+      "/**/*": [
+        "./node_modules/.prisma/**/*",
+        "./node_modules/@prisma/client/**/*",
+      ],
+    },
+  },
 };
 
 export default nextConfig;
