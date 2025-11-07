b13114e1c08ebc205a5050d27bdd51b911b799e1
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2025-11-07 08:47:55 -0800

    Add tests for report filter by status feature

--- __tests__/report.tests.js
+++ __tests__/report.tests.js
@@ -3,16 +3,18 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import Report from "@/app/components/Report";
 
 
-test("selecting completed status filters tasks by completed", async () => {
+
   const mocTasks = {
     debt: [
       {
+        id:1,
         title: "task1",
         createdAt: new Date(),
         isCompleted: true,
         completedAt: new Date(),
       },
       {
+        id:2,
         title: "task2",
         createdAt: new Date(),
         isCompleted: false,
@@ -21,6 +23,7 @@ test("selecting completed status filters tasks by completed", async () => {
     ],
     learning: [
       {
+        id:3,
         title: "task3",
         createdAt: new Date(),
         isCompleted: true,
@@ -28,6 +31,8 @@ test("selecting completed status filters tasks by completed", async () => {
       },
     ],
   };
+
+test("selecting complete status only shows tasks marked completed", () => {
   render(<Report tasks={mocTasks } />)
   fireEvent.change(screen.getByRole("combobox", { name: "Status:" }), {
     target: { value: "complete" },
@@ -36,5 +41,15 @@ test("selecting completed status filters tasks by completed", async () => {
     expect(screen.getByText("task1")).toBeInTheDocument();
     expect(screen.getByText("task3")).toBeInTheDocument();
     expect(screen.queryByText("task2")).not.toBeInTheDocument();
+});
 
+test("selecting incomplete status only shows tasks marked incomplete", () => {
+  render(<Report tasks={mocTasks} />);
+  fireEvent.change(screen.getByRole("combobox", { name: "Status:" }), {
+    target: { value: "incomplete" },
+  });
+
+  expect(screen.getByText("task2")).toBeInTheDocument();
+  expect(screen.queryByText("task1")).not.toBeInTheDocument();
+  expect(screen.queryByText("task3")).not.toBeInTheDocument();
 });
